3fa88f9be59d887e41bac4331f41d7150bf74647
Janu <janu.m.sung@gmail.com>
2021-02-03 19:34:52 -0800

    forgot to change cors for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://paka-lolo.herokuapp.com'
-    origins '*'
+    origins 'https://paka-lolo.herokuapp.com'
+    # origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
